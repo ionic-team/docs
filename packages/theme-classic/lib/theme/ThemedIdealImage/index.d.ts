03a07f3e87d06face28e182eeaf1843d09749077
jared <jaredcbaum@gmail.com>
2023-10-19 17:38:05 -0600

    chore(theme-classic): build to lib

--- packages/theme-classic/lib/theme/ThemedIdealImage/index.d.ts
+++ packages/theme-classic/lib/theme/ThemedIdealImage/index.d.ts
@@ -1,3 +1,31 @@
-import React from 'react';
-import { NavbarLogo } from '@docusaurus/theme-common';
-export default function ThemedIdealImage(props: NavbarLogo): React.JSX.Element;
+import React, { ComponentProps } from 'react';
+import Image from '@theme/IdealImage';
+export declare type SrcType = {
+    width: number;
+    path?: string;
+    size?: number;
+    format?: 'webp' | 'jpeg' | 'png' | 'gif';
+};
+export declare type SrcImage = {
+    height?: number;
+    width?: number;
+    preSrc: string;
+    src: string;
+    images: SrcType[];
+};
+interface ThemedIdealImageProps extends ComponentProps<typeof Image> {
+    src: {
+        default: string;
+    } | {
+        src: SrcImage;
+        preSrc: string;
+    } | string;
+    srcDark?: {
+        default: string;
+    } | {
+        src: SrcImage;
+        preSrc: string;
+    } | string;
+}
+export default function ThemedIdealImage(props: ThemedIdealImageProps): React.JSX.Element;
+export {};
--- packages/theme-classic/lib/theme/ThemedIdealImage/index.js
+++ packages/theme-classic/lib/theme/ThemedIdealImage/index.js
@@ -11,26 +11,39 @@ const clsx_1 = require('clsx');
 const index_module_scss_1 = tslib_1.__importDefault(
   require('./index.module.scss'),
 );
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 function ThemedIdealImage(props) {
   const isBrowser = (0, useIsBrowser_1.default)();
   const {colorMode} = (0, theme_common_1.useColorMode)();
-  const {className, src, srcDark: srcDarkWithoutFallback, ...propsRest} = props;
+  const {className, ...propsRest} = props;
   const clientThemes = colorMode === 'dark' ? ['dark'] : ['light'];
   const renderedSourceNames = isBrowser
     ? clientThemes
     : // We need to render both images on the server to avoid flash
       // See https://github.com/facebook/docusaurus/pull/3730
       ['light', 'dark'];
-  const srcDark =
-    srcDarkWithoutFallback !== null && srcDarkWithoutFallback !== void 0
-      ? srcDarkWithoutFallback
-      : src;
   return react_1.default.createElement(
     react_1.default.Fragment,
     null,
-    renderedSourceNames.map((sourceName) =>
-      react_1.default.createElement(IdealImage_1.default, {
-        img: sourceName === 'light' ? src : srcDark,
+    renderedSourceNames.map((sourceName) => {
+      let key =
+        sourceName === 'light'
+          ? 'src'
+          : `src${capitalizeFirstLetter(sourceName)}`;
+      if (!(key in propsRest)) {
+        key = 'src';
+      }
+      if (typeof propsRest[key] !== 'string' && 'src' in propsRest[key]) {
+        propsRest[key].src = {
+          ...propsRest[key].src,
+          width: propsRest.width,
+          height: propsRest.height,
+        };
+      }
+      return react_1.default.createElement(IdealImage_1.default, {
+        img: propsRest[key],
         key: sourceName,
         className: (0, clsx_1.clsx)(
           index_module_scss_1.default.themedImage,
@@ -38,8 +51,8 @@ function ThemedIdealImage(props) {
           className,
         ),
         ...propsRest,
-      }),
-    ),
+      });
+    }),
   );
 }
 exports.default = ThemedIdealImage;
