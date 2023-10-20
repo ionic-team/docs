983797de1f2d397c1e9326cdd3df8c72ba1c822c
jared <jaredcbaum@gmail.com>
2023-10-20 17:19:39 -0600

    chore(theme-classic): build to lib

--- packages/theme-classic/lib/theme/MDXComponents/index.js
+++ packages/theme-classic/lib/theme/MDXComponents/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, '__esModule', {value: true});
 const tslib_1 = require('tslib');
 const react_1 = tslib_1.__importDefault(require('react'));
 const MDXComponents_1 = tslib_1.__importDefault(
-  require('@theme-original/MDXComponents'),
+  require('@theme-init/MDXComponents'),
 );
 exports.default = {
   ...MDXComponents_1.default,
