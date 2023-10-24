a76c333b2291d5ebcdbbed7ca5c5f0a3456c9ad0
jared <jaredcbaum@gmail.com>
2023-10-24 17:39:57 -0600

    styles(theme-classic): MobileSidebar; back button styles

--- packages/theme-classic/src/theme/Navbar/MobileSidebar/Header/index.tsx
+++ packages/theme-classic/src/theme/Navbar/MobileSidebar/Header/index.tsx
@@ -13,6 +13,8 @@ import IconClose from '@theme/Icon/Close';
 import NavbarLogo from '@theme/Navbar/Logo';
 import clsx from 'clsx';
 
+import styles from './index.module.scss';
+
 function CloseButton() {
   const mobileSidebar = useNavbarMobileSidebar();
   return (
@@ -38,7 +40,7 @@ export default function NavbarMobileSidebarHeader(): JSX.Element {
   return (
     <div className="navbar-sidebar__brand">
       {backButton && (
-        <a {...backButton} className={clsx(backButton.class, 'back-button')}>
+        <a {...backButton} className={clsx(styles.backButton, backButton.class, 'back-button')}>
           <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M16 7H3.85011L9.4502 1.39991L8 0L0 8L8 16L9.39986 14.6L3.85011 9H16V7Z" fill="currentColor"></path>
           </svg>
