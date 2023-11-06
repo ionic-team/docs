36565d28f061fe614166fd85a8a85373263d9f76
jared <jaredcbaum@gmail.com>
2023-11-06 16:39:03 -0700

    chore(theme-classic): build to lib

--- packages/theme-classic/lib/theme/Icon/Language/index.js
+++ packages/theme-classic/lib/theme/Icon/Language/index.js
@@ -21,7 +21,7 @@ function IconLanguage(props) {
     },
     react_1.default.createElement('path', {
       d: 'M1 3H13.1154M7.05764 1V3M10.4232 16.9998L14.4616 7.6665L18.5001 16.9998M11.6641 14.3335H17.259M10.8143 3C10.8143 3 9.79201 6.91667 7.3521 9.875C4.91218 12.8333 2.34606 14.3333 2.34606 14.3333M9.74992 12.3332C9.74992 12.3332 8.27755 11.2082 6.72106 9.20817C5.16456 7.20817 4.36528 5.6665 4.36528 5.6665',
-      stroke: '#92A0B3',
+      stroke: 'currentColor',
       'stroke-width': '2',
       'stroke-linecap': 'round',
       'stroke-linejoin': 'round',
